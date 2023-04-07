fe5d241f2fdce81b406675f1bf45a934b2c7c6ec
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-04-07 16:18:48 -0700

    missed commit hash

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,4 +1,4 @@
 locals {
-  image_tag = "sha256:d7d66fb5797f20460978e2357e1c3766883b1388e2c0fb52efd5d3d13bf726f5"
+  image_tag = "sha256:b27082366fc2fcb1a9c928213ec9ccb51a3aef579dcff6e41d1b4b451c43b73e"
   image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
 }
