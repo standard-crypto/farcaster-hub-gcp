537aebc649dda783ff0c17bc1cee147a3d6b8980
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-05-31 13:24:44 -0700

    update hub to v1.3.0

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,4 +1,4 @@
 locals {
-  image_tag = "sha256:0cf917d9980afb899e84c843551ba0cef700757cdf273c193db4302347121cd3"
+  image_tag = "sha256:893c8d4553350d5482a126e012a05f12ad9fa7f4be364fe8f64679ba0acabd19"
   image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
 }
