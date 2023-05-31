a3ac230fdf4a005fa95c25169f8b32f1b3155b6b
Kevin Ayling <kra4ck@virginia.edu>
2023-05-31 15:20:18 -0700

    add disk snapshot configuration

--- terraform/gke_disk.tf
+++ terraform/gke_disk.tf
@@ -27,3 +27,46 @@ resource "kubernetes_persistent_volume_claim" "farcaster" {
     }
   }
 }
+
+resource "google_compute_disk" "farcaster" {
+  name = "pvc-964b6cad-6a12-49f6-a865-96f46c343a67"
+  description = jsonencode(
+    {
+      "kubernetes.io/created-for/pv/name"       = "pvc-964b6cad-6a12-49f6-a865-96f46c343a67"
+      "kubernetes.io/created-for/pvc/name"      = "farcaster-hub"
+      "kubernetes.io/created-for/pvc/namespace" = "default"
+      "storage.gke.io/created-by"               = "pd.csi.storage.gke.io"
+    }
+  )
+  labels = {
+    "goog-gke-volume"           = ""
+    "goog-k8s-cluster-location" = "us-west1"
+    "goog-k8s-cluster-name"     = "farcaster-hub-cluster"
+    "goog-k8s-node-pool-name"   = "farcaster-hub-node-pool"
+  }
+}
+
+resource "google_compute_resource_policy" "farcaster-snapshot-policy" {
+  name   = "farcaster-snapshot-policy"
+  region = "us-west1"
+  snapshot_schedule_policy {
+    schedule {
+      weekly_schedule {
+        day_of_weeks {
+          day        = "SUNDAY"
+          start_time = "04:00"
+        }
+      }
+    }
+    retention_policy {
+      max_retention_days    = 60
+      on_source_disk_delete = "APPLY_RETENTION_POLICY"
+    }
+  }
+}
+
+resource "google_compute_disk_resource_policy_attachment" "farcaster-snapshot-policy-attachment" {
+  name = google_compute_resource_policy.farcaster-snapshot-policy.name
+  disk = google_compute_disk.farcaster.name
+  zone = google_compute_disk.farcaster.zone
+}
