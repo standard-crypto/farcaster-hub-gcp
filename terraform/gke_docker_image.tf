842815c88ff8a5614f2edc78732d0332ef395856
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-06-01 13:26:59 -0700

    remove unnecessary admin-server-disabled flag

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,4 +1,4 @@
 locals {
-  image_tag = "sha256:04064d3cfb88f6edb0bde5b76dd447393a40e30941d2c7be3fc0956e80df28d5"
+  image_tag = "sha256:ade02555b6f0344372d35965fc8dbbc6f78a0f383f8ec46fb1101b2c71777094"
   image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
 }
