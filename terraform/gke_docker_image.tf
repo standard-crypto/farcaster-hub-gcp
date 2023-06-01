bad91738026a3472de309733767374e94338fd94
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-06-01 13:22:14 -0700

    update docker image

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,4 +1,4 @@
 locals {
-  image_tag = "sha256:893c8d4553350d5482a126e012a05f12ad9fa7f4be364fe8f64679ba0acabd19"
+  image_tag = "sha256:04064d3cfb88f6edb0bde5b76dd447393a40e30941d2c7be3fc0956e80df28d5"
   image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
 }
