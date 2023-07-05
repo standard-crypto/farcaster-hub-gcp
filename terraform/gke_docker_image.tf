e23e7546e8b876ce233dc6ac61452619e55afc04
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-07-05 16:08:53 -0700

    hubble v1.3.3

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,4 +1,4 @@
 locals {
-  image_tag = "sha256:7c037ec53b5e32cc33154575a73139f7c679faa13b12eb4785768a3d8cdc80e0"
+  image_tag = "sha256:38d9fd9d0c716504b5ad2a8fcf14e0f22adc74eb2cb70b19299acdab91fcab0a"
   image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
 }
