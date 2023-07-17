90c2d981340af4952a3fa9779e0b96bffb0dcd93
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-07-17 10:14:18 -0700

    hub v1.4.2

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,5 +1,5 @@
 locals {
-  image_tag            = "sha256:5b78e627fd71b349c9e607dc4ca80005dcbe7fb6e4be4eb4ef85a2e01f826bb8"
+  image_tag            = "sha256:dc763b68cf852b113790bf3dd51fd8e739c1f94da48175865a9493d242ac1ef6"
   image                = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
   prometheus-image_tag = "sha256:e9fb9c766dc649ca8db992f95fd1dd97d5cf7c5bc2fc2dfc67288e63e85d9958"
   prometheus-image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/prometheus@${local.prometheus-image_tag}"
