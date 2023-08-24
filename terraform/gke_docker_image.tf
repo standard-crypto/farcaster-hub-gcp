7aef34a0beeeed4787c8e216f89399a2518e4c43
Kevin Ayling <kra4ck@virginia.edu>
2023-08-24 11:26:19 -0700

    update hubble to v1.5.0

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,5 +1,5 @@
 locals {
-  image_tag            = "sha256:449368163b6fc653d3a45447d55318e694e7ba74cb86d989d154d7ee0022fd63"
+  image_tag            = "sha256:531c37ae9fac7f1ccdeef34d07238068c7731e6e8abb471b810ba320e9dac05c"
   image                = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/farcaster-hubble@${local.image_tag}"
   prometheus-image_tag = "sha256:bffd8d08241fea683d3f0270fd6d4c9dd3d822e5c0042c58e6a2936c2393d300"
   prometheus-image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/prometheus@${local.prometheus-image_tag}"
