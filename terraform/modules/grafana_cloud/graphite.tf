ff940e6afc2be2d93d421820d85472f78c523211
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-09-25 15:00:05 -0700

    wip

--- terraform/modules/grafana_cloud/graphite.tf
+++ terraform/modules/grafana_cloud/graphite.tf
@@ -1,6 +1,8 @@
 resource "grafana_data_source" "graphite" {
-  provider = grafana.farcaster_stack
-  type     = "graphite"
-  name     = "Graphite"
-  url      = var.graphite_url
+  provider   = grafana.farcaster_stack
+  depends_on = [grafana_cloud_stack_service_account_token.cloud_sa]
+
+  type = "graphite"
+  name = "Graphite"
+  url  = var.graphite_url
 }
