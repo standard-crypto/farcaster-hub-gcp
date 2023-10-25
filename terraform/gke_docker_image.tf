c6ff0d118e44d0f9e47fe0b9f4493719d872816a
piratekev <93568025+piratekev@users.noreply.github.com>
2023-10-25 14:41:41 -0700

    Remove custom prometheus stack (#33)
    * Remove custom prometheus stack
    
    * Update gke_docker_image.tf

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,8 +1,4 @@
 locals {
-  prometheus-image_tag    = "sha256:bffd8d08241fea683d3f0270fd6d4c9dd3d822e5c0042c58e6a2936c2393d300"
-  prometheus-image        = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/prometheus@${local.prometheus-image_tag}"
-  grpc-exporter-image_tag = "sha256:5289fa51f3b324f23adfc1fb675d6da20bc330f3d793d4b5b70f3e7b18aff4ba"
-  grpc-exporter-image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/grpc-exporter@${local.grpc-exporter-image_tag}"
   nginx-image_tag         = "sha256:d7022428caf43abd552355468dccb98d934905df536a9278400e3df3ed93ea23"
   nginx-image             = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/nginx@${local.nginx-image_tag}"
 }
