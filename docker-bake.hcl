1accdf978697c29c7afb672b376cfb6b572d4b20
Kevin Ayling <kra4ck@virginia.edu>
2023-10-25 14:21:27 -0700

    Remove custom prometheus stack

--- docker-bake.hcl
+++ docker-bake.hcl
@@ -1,20 +1,7 @@
 group "default" {
     targets = [
-        "prometheus",
-        "grpc-exporter"
+        "nginx"
     ]
-}
-
-target "prometheus" {
-    tags = ["us-west1-docker.pkg.dev/sc-farcaster/docker/prometheus"]
-    platforms = ["linux/amd64"]
-    context = "./docker/prometheus"
-}
-
-target "grpc-exporter" {
-    tags = ["us-west1-docker.pkg.dev/sc-farcaster/docker/grpc-exporter"]
-    platforms = ["linux/amd64"]
-    context = "./docker/grpc-exporter"
 }
 
 target "nginx" {
--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,8 +1,4 @@
 locals {
-  prometheus-image_tag    = "sha256:bffd8d08241fea683d3f0270fd6d4c9dd3d822e5c0042c58e6a2936c2393d300"
-  prometheus-image        = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/prometheus@${local.prometheus-image_tag}"
-  grpc-exporter-image_tag = "sha256:5289fa51f3b324f23adfc1fb675d6da20bc330f3d793d4b5b70f3e7b18aff4ba"
-  grpc-exporter-image     = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/grpc-exporter@${local.grpc-exporter-image_tag}"
   nginx-image_tag         = "sha256:527c3b1ff8f9fbc46a0f70c6d75f3cfdf210a123a7d46f0b76eeaf454b29b9c3"
   nginx-image             = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/nginx@${local.nginx-image_tag}"
 }
