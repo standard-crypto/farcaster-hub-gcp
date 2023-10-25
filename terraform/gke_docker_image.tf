5576c3443b554d1b92bec2d40910b262b2c6600c
piratekev <93568025+piratekev@users.noreply.github.com>
2023-10-25 14:40:08 -0700

    Update gke_docker_image.tf

--- terraform/gke_docker_image.tf
+++ terraform/gke_docker_image.tf
@@ -1,4 +1,4 @@
 locals {
-  nginx-image_tag         = "sha256:527c3b1ff8f9fbc46a0f70c6d75f3cfdf210a123a7d46f0b76eeaf454b29b9c3"
+  nginx-image_tag         = "sha256:d7022428caf43abd552355468dccb98d934905df536a9278400e3df3ed93ea23"
   nginx-image             = "${var.region}-docker.pkg.dev/${var.project-id}/${google_artifact_registry_repository.artifact-registry.repository_id}/nginx@${local.nginx-image_tag}"
 }
