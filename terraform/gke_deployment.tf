b892265b0dfe5db521678442a89a9656ceb85f1c
Kevin Ayling <93568025+piratekev@users.noreply.github.com>
2024-01-06 18:06:42 +0000

    Update Hubble version to 1.9.1

--- terraform/gke_deployment.tf
+++ terraform/gke_deployment.tf
@@ -28,7 +28,7 @@ resource "kubernetes_deployment" "farcaster" {
           run_as_user = "1000"
         }
         container {
-          image = "farcasterxyz/hubble:1.9.0"
+          image = "farcasterxyz/hubble:1.9.1"
           name  = "${var.name}-image"
           volume_mount {
             name       = var.name
