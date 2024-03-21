956c03d8451ee348969c8a0f197c8540f10fa1ae
Kevin Ayling <93568025+piratekev@users.noreply.github.com>
2024-03-21 18:07:28 +0000

    Update Hubble version to 1.11.0

--- terraform/gke_deployment.tf
+++ terraform/gke_deployment.tf
@@ -27,7 +27,7 @@ resource "kubernetes_deployment" "farcaster" {
           fs_group = "1000" # user 'node'
         }
         container {
-          image = "farcasterxyz/hubble:1.10.11"
+          image = "farcasterxyz/hubble:1.11.0"
           name  = "${var.name}-container"
           security_context {
             allow_privilege_escalation = false
